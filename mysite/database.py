b0508256808a8d192d927ed7ec6721e01d48cd23
Jacob Sohl <jacobsohl@gmail.com>
2018-06-02 00:50:45 -0700

    accidently deleted connection function :(

--- mysite/database.py
+++ mysite/database.py
@@ -3,12 +3,15 @@ import config
 
 db = config.database
 def init_db():
-    cn = sqlite3.connect(db)
+    cn = connect_db()
     with cn:
         cr = cn.cursor()
         cr.execute('CREATE TABLE IF NOT EXISTS clients (ip TEXT, hostname TEXT)')
     cn.close()
     print('initialized db')
+
+def connect_db():
+    return sqlite3.connect(db)
 
 if __name__ == "__main__":
     init_db()
