b0d377d2379a6a0e828aa0e724618e669c11daa8
Jacob Sohl <jacobsohl@gmail.com>
2018-06-02 00:46:29 -0700

    clean up. i'm not really sure what i'm doing

--- mysite/database.py
+++ mysite/database.py
@@ -6,26 +6,9 @@ def init_db():
     cn = sqlite3.connect(db)
     with cn:
         cr = cn.cursor()
-        cr.execute('CREATE TABLE IF NOT EXISTS clients (ip text, hostname text)')
+        cr.execute('CREATE TABLE IF NOT EXISTS clients (ip TEXT, hostname TEXT)')
     cn.close()
     print('initialized db')
-
-def connect_db():
-    return sqlite3.connect(db)
-
-def insert(table,record):
-    conn = sqlite3.connect(db)
-    with conn:
-        conn.execute("INSERT INTO ? values (','.join(['?']*len(record),)",(table,*record))
-
-def update(table,record):
-    pass
-
-def get_record(table,query):
-    pass
-
-def remove(table,record):
-    pass
 
 if __name__ == "__main__":
     init_db()
