77cfb4b76a64bfa85bcb339ed2d2fc86bc0b1837
Jacob Sohl <jacobsohl@gmail.com>
2018-06-08 01:06:40 -0700

    strip / from endpoints. add create page for notes

--- mysite/routes.py
+++ mysite/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from mysite import app
 from mysite.models import User
 
@@ -8,7 +8,17 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/blog/')
+@app.route('/blog')
 def blog():
     # if user is logged in should also be able to create posts
     return render_template('blog.html')
+
+@app.route('/create', methods=['GET','POST'])
+def create():
+    if request.method == 'POST':
+        if request.form.get('title') and request.form.get('content'):
+            return render_template('entry.html')
+        else:
+            return render_template('error.html', error="failure posting")
+    else:
+        return render_template('create.html')
