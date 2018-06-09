96044478b34a3c7cb938a975c05978b791ae087d
Jacob Sohl <jacobsohl@gmail.com>
2018-06-08 18:31:58 -0700

    continuous improvements
    * admin page to init db
    * learning bootstrap
    * formatting the page
    * revise db model

--- mysite/models.py
+++ mysite/models.py
@@ -4,7 +4,7 @@ from mysite import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -12,14 +12,14 @@ class User(db.Model):
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(160), unique=True, nullable=False)
-    author = db.Column(db.String(120), unique=True, nullable=False)
-    series = db.Column(db.String(160), unique=True, nullable=False)
-    genre = db.Column(db.String(80), unique=True, nullable=False)
+    title = db.Column(db.String(160), nullable=False)
+    author = db.Column(db.String(120), nullable=False)
+    series = db.Column(db.String(160))
+    genre = db.Column(db.String(80))
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(160), unique=True, nullable=False)
-    content = db.Column(db.Text, unique=True, nullable=False)
+    title = db.Column(db.String(160), nullable=False)
+    content = db.Column(db.Text, nullable=False)
 
--- mysite/routes.py
+++ mysite/routes.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request
-from mysite import app
-from mysite.models import User
+from mysite import app, db
+from mysite.models import User, Post
 
 @app.route('/')
 def index():
 #    return('index')
     return render_template('index.html')
 
+@app.route('/admin', methods=['GET','POST'])
+def admin():
+    msg = ''
+    if request.method == 'POST':
+        if request.form.get('submit') == 'init_db':
+            db.create_all()
+            msg = "DB initialized..."
+    return render_template('admin.html',msg=msg)
 
 @app.route('/activity')
 def activity():
@@ -21,6 +29,9 @@ def blog():
 def create():
     if request.method == 'POST':
         if request.form.get('title') and request.form.get('content'):
+            p = Post(title=request.form.get('title'), content=request.form.get('content'))
+            db.session.add(p)
+            db.session.commit()
             return render_template('entry.html')
         else:
             return render_template('error.html', error="failure posting")
