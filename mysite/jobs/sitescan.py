465a05f9e39bf4a9a6a603bb03707219e88a93db
Jacob Sohl <jacobsohl@gmail.com>
2018-06-13 22:57:14 -0700

    clean up only way and unused imports

--- mysite/jobs/sitescan.py
+++ mysite/jobs/sitescan.py
@@ -4,9 +4,7 @@ Job to scan network and store list of members in database
 import sys
 import datetime
 import nmap
-from sqlalchemy import create_engine
 from mysite import db
-import mysite
 from mysite.models import Host, Subnet
 
 def scan(scanrange):
@@ -18,20 +16,6 @@ def scan(scanrange):
     scanner.scan(hosts=scanrange, arguments='-sP')
     host_list = [(x, scanner[x]['hostnames'][0]['name']) for x in scanner.all_hosts()]
     return host_list
-
-
-def update_db(host_list):
-    '''
-    update db using builtin sqlite
-    '''
-    conn = mysite.database.connect_db()
-    with conn:
-        cur = conn.cursor()
-        cur.execute('DROP TABLE IF EXISTS clients')
-        conn.commit()
-        cur.execute('CREATE TABLE clients (ip text, hostname text)')
-        for host in host_list:
-            conn.execute("INSERT INTO clients VALUES(?,?)", (host))
 
 
 def update_hosts(host_list):
